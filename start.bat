2d27c0d25d17551455f1a1f55fb7eeb041a6918a
zByDiegoM.T <97636244+zByDiegoMT@users.noreply.github.com>
2022-04-21 16:04:53 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,23 +1,27 @@
 @echo off
-chcp 65001
+chcp 65001 >nul
 :again
 cls
-color c
-title Diego Discord Cloner
-echo Porfavor, logueese usando usuario y contrasena Pls Login With Username and Password
+color 2
+title Diego Discord Server Cloner
 echo.
-echo Bienvenido Welcome %USERNAME%
-echo.
-echo. 
+type LOGO-NO-TOCAR-O-BUG.txt
 echo.
 echo.
-set /p user=Nombre de Usuario - Username: 
 echo.
-set /p pass=Contrasena - Password: 
+echo [40;36mBIENVENIDO -- WELCOME %USERNAME%
+echo.
+echo [40;37m-------------------------------------------
+echo [40;37m-------------------------------------------
+echo.
+set /p user=[40;36mNombre de Usuario - Username: 
+echo.
+set /p pass=[40;36mContrasena - Password: 
 if %user% == zbydiegomt if %pass% == diego-cloner-v2 goto main
-echo Invalid password or username, try...
+echo [40;31mInvalid password or username, try...
 timeout 3 >nul
 goto again
 :main
+cls
 node index.js
 pause
